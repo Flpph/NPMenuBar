3cb519e303e139e6fd0879ef5bf921dbf6ee30ed
Attila Szél <szelattila200106@gmail.com>
2022-04-05 20:04:08 +0200

    Initial Commit

---
+++ MacMenuApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MacMenuApp
+//
+//  Created by Attila Szél on 2022. 04. 05..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MacMenuApp/MacMenuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacMenuAppApp.swift
+//  MacMenuApp
+//
+//  Created by Attila Szél on 2022. 04. 05..
+//
+
+import SwiftUI
+
+@main
+struct MacMenuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
